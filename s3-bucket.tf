f384b7652fd32d280f4daaa529f1c72e2c7a5410
Michal Rybinski <michal.rybinski.uk@gmail.com>
2021-10-14 11:27:31 +0000

    Added s3 bucket module - fixing

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,6 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
+module "s3-bucket" {
+  source  = "app.terraform.io/michal_rybinski_demo/s3-bucket/aws"
+  version = "2.2.0"
   bucket_prefix = "demo-bucket"
   acl    = "private"
 
